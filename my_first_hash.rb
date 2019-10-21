2d7dcfbe01e1431eb45458c44adaa60edfacf5ae
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-21 03:27:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,14 +17,14 @@ end
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
-
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
   # add 2 muskets to the shipping_manifest hash below
 
 
