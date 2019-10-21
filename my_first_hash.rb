c31114f135e5a4b3a6d18b278d2da56881113e5e
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-21 03:06:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+def my_hash []
+  your_hash = {"name" => "Gina", "city" => "New York"}
+ end
 
 
 def shipping_manifest
